881968058e405f06ff3e6fbb9bdc58301640989c
luka-mimi <luka.cat.mimi@gmail.com>
2026-04-13 15:48:19 +0800

    ci: 升级cli和依赖

--- test/moduleLoadTest.ts
+++
@@ -1,4 +0,0 @@
-import ModuleLoadUtils from '../nodes/help/utils/moduleLoadUtils';
-
-const path = __dirname + '/../dist/nodes/WechatOfficialAccountNode/resource';
-console.log(ModuleLoadUtils.loadModules(path, '*.js'));
